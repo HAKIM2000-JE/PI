150bc0bb91c51ea44a692f7d1c2e0a457c9a30ba
Jebabra Abdelhakim <59029617+HAKIM2000-JE@users.noreply.github.com>
2021-06-06 11:38:41 +0100

    Backend add with csv

--- DeptInfoBackend-master/app.js
+++ DeptInfoBackend-master/app.js
@@ -5,8 +5,11 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
+const Papa = require("papaparse")
+
 const db = require('./models');
 const document = require('./models/document');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +21,11 @@ var departementRouter = require('./routes/departement');
 var filiereRouter = require('./routes/filiere');
 
 var uploadRouter = require('./routes/upload');
+
+const CSVToJSON = require("csvtojson");
+const JSONToCSV = require("json2csv").parse;
+const FileSystem = require("fs");
+const { Console } = require('console');
 
 var app = express();
 
@@ -78,6 +86,111 @@ app.post('/upload', (req, res, next) => {
     });
 
 });
+
+
+
+app.post('/EnseignantCsv', (req, res, next)=>{
+    let file = req.files.file
+
+    console.log({file:file})
+    
+    // console.log("sj kjsdflkj kjdskjf dsfkljdalkjh")
+
+
+
+
+    file.mv(`public/documents__tableau__affichage/user.csv`, function (err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json({ file: `public/documents__tableau__affichage/user.csv` });
+
+    
+
+        // Importing csv-parser into fsdata
+        const csvdata = require('csv-parser')
+        const fsdata = require('fs');
+      
+      
+
+        CSVToJSON().fromFile(`public/documents__tableau__affichage/user.csv`).then(source => {
+            console.log(source)
+             source.map((user)=>{
+                 db.enseignant.create({
+                     nom: user.nom,
+                     prenom: user.prenom,
+                     EnseignantPassword: user.password,
+                     email: user.email,
+                     specialite: user.specialite,
+                     grade: user.grade,
+
+                 }).then((doc) => res.send(doc));
+
+             })
+            // source.push({
+            //     "sku": "34890",
+            //     "title": "Fortnite",
+            //     "hardware": "Nintendo Switch",
+            //     "price": "00.00"
+            // });
+
+        })
+
+    })
+
+})
+
+
+app.post('/EtudiantCsv', (req, res, next) => {
+    let file = req.files.file
+
+    console.log({ file: file })
+
+    // console.log("sj kjsdflkj kjdskjf dsfkljdalkjh")
+
+
+
+
+    file.mv(`public/documents__tableau__affichage/user.csv`, function (err) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.json({ file: `public/documents__tableau__affichage/user.csv` });
+
+
+
+        // Importing csv-parser into fsdata
+        const csvdata = require('csv-parser')
+        const fsdata = require('fs');
+
+
+
+        CSVToJSON().fromFile(`public/documents__tableau__affichage/user.csv`).then(source => {
+            console.log(source)
+            source.map((user) => {
+                db.etudiant.create({
+                    nom: user.nom,
+                    prenom: user.prenom,
+                    EtudiantPassword: user.password,
+                    email: user.email,
+                    matricule: user.matricule,
+                    genie: user.genie,
+
+                }).then((doc) => res.send(doc));
+
+            })
+            // source.push({
+            //     "sku": "34890",
+            //     "title": "Fortnite",
+            //     "hardware": "Nintendo Switch",
+            //     "price": "00.00"
+            // });
+
+        })
+
+    })
+
+})
 
 
 // catch 404 and forward to error handler
